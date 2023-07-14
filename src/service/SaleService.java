3c6b84726ae0d9d61bae5c08e058db59aa034335
sangyeobhan <sangyeobhandev@gmail.com>
2023-07-14 09:38:00 -0400

    Reduced redundant code

--- src/service/SaleService.java
+++ src/service/SaleService.java
@@ -10,17 +10,16 @@ public class SaleService {
     DiscountPayment discountPayment = new DiscountPayment();
 
     public void processPayment(double price, Customer customer){
+        customer.printCustomer();
         if(customer.getPaymentPolicy() == PaymentPolicy.BONUS) {
-            customer.printCustomer();
             System.out.println("결제 방식: 보너스 결제 방식");
             System.out.printf("보너스: %s\n", (int) bonusPayment.getBonusAmount(price,customer));
             System.out.printf("결제 금액: %s\n", (int) bonusPayment.amountPaid(price,customer));
-            System.out.println("=========================");
         } else {
-            customer.printCustomer();
             System.out.println("결제 방식: 할인 결제 방식");
             System.out.printf("결제 금액: %s\n", (int) discountPayment.amountPaid(price,customer));
-            System.out.println("=========================");
         }
+        System.out.println("=========================");
+
     }
 }
